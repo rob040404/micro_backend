93b55bd375186101999c89d5d72c76b1a9e56f90
rob040404 <robert.gr704@gmail.com>
2025-11-20 16:33:59 +0100

    Controller for follow accesptance or rejection created

--- SocialMicroservice/src/main/java/com/social/SocialMicroservice/dto/AnswerFollowRequestsDTO.java
+++ SocialMicroservice/src/main/java/com/social/SocialMicroservice/dto/AnswerFollowRequestsDTO.java
@@ -1,4 +1,16 @@
 package com.social.SocialMicroservice.dto;
 
+import com.social.SocialMicroservice.entities.FollowStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+@AllArgsConstructor @Getter @Setter
 public class AnswerFollowRequestsDTO {
+
+    private UUID followRequestId;
+    private boolean answer;
+
 }
